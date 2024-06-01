bbcd5a613389a37166846d60395ca2cce07b54a9
deveshbis <deveshbiswas.cse@gmail.com>
2024-06-01 19:30:13 +0600

    created Article Details page

--- src/Pages/Articles/TrendingArticles.jsx
+++ src/Pages/Articles/TrendingArticles.jsx
@@ -14,16 +14,16 @@ import { Link } from 'react-router-dom';
 const TrendingArticles = () => {
     const [news, setnews] = useState([]);
     useEffect(()=>{
-        fetch("articles.json")
+        fetch(`${import.meta.env.VITE_API_URL}/articles`)
         .then(res => res.json())
         .then(data => setnews(data))
-    } ,[])
+    }, [])
     console.log(news);
 
     const truncateContent = (content, maxLength) => {
         const words = content.split(' ');
         if (words.length > maxLength) {
-            return words.slice(0, maxLength).join(' ') + '...'; // Truncate content and add ellipsis
+            return words.slice(0, maxLength).join(' ') + '...';
         } else {
             return content;
         }
@@ -33,14 +33,14 @@ const TrendingArticles = () => {
         
         {
             
-            news.map(aNews=><SwiperSlide key={aNews.id}>
+            news.map(aNews=><SwiperSlide key={aNews._id}>
                 <div className='space-y-3'>
                     <img src={aNews.image} className='h-full w-full' alt="" />
                     <h2 className="text-4xl">{aNews.headline}</h2>
                     <h4 className="text-xl">Date: {aNews.date}</h4>
                     <p>{truncateContent(aNews.content, 50)}</p>
-                    {aNews.content.split(' ').length > 50 && ( // Check if content exceeds 50 words
-                            <Link to={`/article/${aNews.id}`} className="text-blue-500">See more</Link> // Link to new page
+                    {aNews.content.split(' ').length > 50 && ( 
+                            <Link to={`/article/${aNews._id}`} className="text-blue-500">See more</Link> 
                         )}
                 </div>
 
--- src/Pages/ArticlesViewDetails/ArticlesViewDetails.jsx
+++ src/Pages/ArticlesViewDetails/ArticlesViewDetails.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 const ArticlesViewDetails = () => {
-    
+    const { id } = useParams()
+    const [details, setDetails] = useState({});
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/singleDetails/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setDetails(data)
+            })
+    }, [id]);
+    console.log(details);
     return (
         <div>
-            
+            <div className="mt-5 space-y-3">
+               
+                <h2 className="text-5xl font-extrabold">{details.headline}</h2>
+                <img src={details.image} className='h-full w-full' alt="" />
+                <p><span className="text-green-500 text-2xl font-bold">Date:</span> {details.date}</p>
+                <p><span className="text-red-500 text-2xl font-bold">Publisher:</span> {details.publisher}</p>
+                <p> <span className="text-blue-500 text-2xl font-bold">Content:</span> {details.content}</p>
+            </div>
         </div>
     );
 };
 
 export default ArticlesViewDetails;
+
