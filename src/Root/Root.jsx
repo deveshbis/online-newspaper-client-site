840370ee14511c0c77faeccf0cce69f7a1ddcd88
deveshbis <deveshbiswas.cse@gmail.com>
2024-08-24 17:09:38 +0600

    added breaking news section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Navbar/Navbar";
 import Footer from "../Pages/Footer/Footer";
 import Header from "../Shared/Header/Header";
+import BreakingNews from "../Shared/Header/BreakingNews";
 
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
